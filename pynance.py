d25283f8cd02a4432860fca72ed8d7c60c46ec8a
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-01 05:01:10 +0200

    part 3 commit

--- pynance.py
+++ pynance.py
@@ -44,3 +44,18 @@ df = pd.read_csv('tsla.csv', parse_dates = True, index_col = 0)
 print(df[['Open', 'High']].head()) # Afficher les colonnes Open et High
 df['Adj Close'].plot
 plt.show()
+
+# Calcul de la moyenne mobile sur 100 Jours  (analyse sur le long terme)
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+# Suppression des valeurs manquantes
+df.dropna(inplace=True)
+print(df.head())
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show()
