d1be1516f7ea594446924414d3304e796414f817
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-11 18:08:11 +0200

    part 10

--- ml.py
+++ ml.py
@@ -1,13 +1,23 @@
+from collections import Counter
+#Bibliothèques pour manipulation de données
 import numpy as np
 import pandas as pd
+#Sauvegarde des objets Python
 import pickle
+#Modèles de ml
+from sklearn import svm, neighbors
+#Learning
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
 
 def process_data_for_labels(ticker):
+    #Charge les données du fichier et crée la variable 7 jour
     hm_days = 7
     df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
     tickers = df.columns.values.tolist()
     df.fillna(0 , inplace = True)
-
+    #calcul pour chaque jour i
     for i in range(1, hm_days+1):
         df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
 
@@ -15,3 +25,72 @@ def process_data_for_labels(ticker):
     return tickers, df
 
 process_data_for_labels('XOM')
+#Fonction déterminant s'il faut acheter vendre ou conserver
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > 0.028:
+            #Si variation > 2.5% --> Achat
+            return 1
+        if col < -0.027:
+            #Si variation < 2.5% --> Vente
+            return -1
+        #Conservation
+    return 0
+
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+    #création de colonne
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)],
+                                               ))
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace = True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+
+
+def do_ml(ticker):
+    X, y, df = extract_featuresets(ticker)
+
+    X_train, X_test, y_train, y_test =train_test_split(X,
+                                                                         y,
+                                                                         test_size = 0.25)
+    
+    # clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print('Accuracy', confidence)
+    predictions = clf.predict(X_test)
+    print('Predicted spread:', Counter(predictions))
+
+    return confidence
+
+do_ml('BAC')
+
+
